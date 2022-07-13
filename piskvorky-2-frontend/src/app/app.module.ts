dfd3bd5d5230f2586fd829b8df6b03a934469c5f
Jan Janeczko <janeczko.jan@hotmail.com>
2022-07-13 20:33:23 +0200

    cleanup frontend and add state management

--- piskvorky-2-frontend/src/app/app.module.ts
+++ piskvorky-2-frontend/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+
+
+const reducers = {
+	router: routerReducer
+};
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		StoreModule.forRoot(reducers, {}),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+		StoreRouterConnectingModule.forRoot()
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
